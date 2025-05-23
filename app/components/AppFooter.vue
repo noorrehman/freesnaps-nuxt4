e42c5559c0387328ff0ba58f4e19498e70324c29
Noor Rehman <786uog@gmail.com>
2025-05-23 19:46:34 +0500

    First Commit

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-center ">
+    <h2 class="text-3xl font-bold text-orange-500">This my Footer</h2>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center py-12">
+    <h2 class="text-3xl font-bold text-sky-500">This my Home Page</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+
+</script>
